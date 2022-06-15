efa04cc6c92759d88e79937af1840716f80df2cc
Friederike L. Bauer <52316159+friederikelbauer@users.noreply.github.com>
2022-06-15 17:23:18 +0200

    basic setup Participation Page

--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -9,8 +9,10 @@ from otree.api import (
     currency_range,
 )
 
-author = 'your names and team objective go here'
-doc = 'Your app description goes here'
+author = '''Team Social Movement, 
+Friederike Bauer, Leslie Fischer, Lina Jeannot, Freya Moßig, Pascal Mounchid, Anna Schleiter Nielsen'''
+doc = '''Social Movements:
+How does social pressure impact the probability of joining a demonstration?'''
 
 class Constants(BaseConstants):
     name_in_url = 'survey-example'
@@ -21,26 +23,20 @@ class Subsession(BaseSubsession):
     pass
 
 class Group(BaseGroup):
-    counter = models.IntegerField(initial = 0)
-    #this is how you can implement variables that can be used by every player
-    #they are called group variables and useful for example when quota checking
+    pass
 
 
 class Player(BasePlayer):
-    #this is the most important feature of this file. We can collect all the variables used on the html pages here
-    
-#The Variables are structured on the base of pages
-    entry_question = models.StringField(blank = True) #this is an optional field through blank = True
-    age_question = models.IntegerField(max=110, min=1)  #we can also have max and min guidelines
-    gender = models.IntegerField(initial=-999)  #we can add an initial value 
-    hidden_input = models.IntegerField(initial=50, blank=True)
+    #ParticipationPage
+    participation = models.IntegerField(initial=-999)
+    topic_relevance = models.IntegerField(initial = -999)
+    familiar_people = models.IntegerField(initial=-999)
+    booked = models.IntegerField(initial=-999)
+    transportation = models.IntegerField(initial = -999)
+    reach = models.IntegerField(initial=-999)
+    crowd = models.IntegerField(initial=-999)
+    polarization = models.IntegerField(initial=-999)
+    riots = models.IntegerField(initial=-999)
+    participation_else = models.StringField(blank=True)
 
-
-    #custom error message
-        #has to: 
-        #1) be in the class Player (important to indent the right way)
-        #2) have a specific name "variablename"_error_message
-    def age_question_error_message(player, value):
-        if value > 50:
-            return 'You are too old. Are you sure you are taking this course?'
                         
