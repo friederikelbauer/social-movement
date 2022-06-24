7a1f2866d4f31b908440e4e3a189b498b394f14c
Friederike L. Bauer <52316159+friederikelbauer@users.noreply.github.com>
2022-06-24 19:37:28 +0200

    timestamps, new page for variables

--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -35,11 +35,15 @@ class Group(BaseGroup):
 class Player(BasePlayer):
 
 # Social Movement (sm) variables
-    #general varibles
+    #general variables
     sm_group_assignment = models.IntegerField(initial=-999)
+    #Welcome
+    sm_time_welcome = models.StringField(initial=-999)
     #TreatmentPage
+    sm_time_treatmentpage = models.StringField(initial=-999)
     sm_participation = models.IntegerField(initial=-999)
     #ParticipationPage
+    sm_time_participationpage = models.StringField(initial=-999)
     sm_topic_relevance = models.IntegerField(initial = -999)
     sm_familiar_people = models.IntegerField(initial=-999)
     sm_booked = models.IntegerField(initial=-999)
@@ -50,10 +54,22 @@ class Player(BasePlayer):
     sm_riots = models.IntegerField(initial=-999)
     sm_participation_else = models.StringField(blank=True) #voluntary
     #PoliticalPage
-    sm_political_participation = models.IntegerField(initial=-999)
-    sm_political_influence = models.IntegerField(initial=-999)
-    sm_political_chance = models.IntegerField(initial=-999)
+    sm_time_politicalpage = models.StringField(initial=-999)
+    sm_missing_knowledge = models.IntegerField(initial=-999)
+    sm_own_knowledge = models.IntegerField(initial=-999)
+    sm_general_knowledge = models.IntegerField(initial=-999)
     sm_future = models.IntegerField(initial=-999)
     sm_unsure = models.IntegerField(initial=-999)
     sm_olddays = models.IntegerField(initial=-999)
+    #DemonstrationPage
+    sm_time_demonstrationpage = models.StringField(initial=-999)
+    sm_demonstration_change = models.IntegerField(initial=-999)
+    sm_demonstration_democracy = models.IntegerField(initial=-999)
+    sm_demonstration_views = models.IntegerField(initial=-999)
+    sm_politicians = models.IntegerField(initial=-999)
+    sm_influence = models.IntegerField(initial=-999)
+    sm_interests = models.IntegerField(initial=-999)
+    #EndPage
+    sm_time_endpage = models.StringField(initial=-999)
+
                         
--- otree-example/survey_example_appfolder/pages.py
+++ otree-example/survey_example_appfolder/pages.py
@@ -6,31 +6,37 @@ from .models import Constants, Player
 
 class Welcome(Page):
     form_model = Player
-    form_fields = []
+    form_fields = ['sm_time_welcome']
 
 class TreatmentPage(Page):
     form_model = Player
-    form_fields = ['sm_participation']
+    form_fields = ['sm_time_treatmentpage', 'sm_participation']
 
     def vars_for_template(self):
         return {'sm_group_assignment': safe_json(self.player.sm_group_assignment)}
 
 class ParticipationPage(Page):
     form_model = Player
-    form_fields=['sm_topic_relevance', 'sm_familiar_people', 'sm_booked', 'sm_transportation', 'sm_reach', 'sm_crowd', 
+    form_fields=['sm_time_participationpage', 'sm_topic_relevance', 'sm_familiar_people', 'sm_booked', 'sm_transportation', 'sm_reach', 'sm_crowd', 
     'sm_polarization', 'sm_riots', 'sm_participation_else']
 
 class PoliticalPage(Page):
     form_model = Player
-    form_fields=['sm_political_participation', 'sm_political_influence', 'sm_political_chance',
+    form_fields=['sm_time_politicalpage', 'sm_missing_knowledge', 'sm_own_knowledge', 'sm_general_knowledge',
     'sm_future', 'sm_unsure', 'sm_olddays']
 
-class EndPage(Page):
-
+class DemonstrationPage(Page):
     form_model = Player
+    form_fields=['sm_time_demonstrationpage', 'sm_demonstration_change', 'sm_demonstration_democracy', 'sm_demonstration_views',
+    'sm_politicians', 'sm_influence', 'sm_interests']
+
+class EndPage(Page):
+    form_model = Player
+    form_fields=['sm_time_endpage']
 
 page_sequence = [Welcome,
                 TreatmentPage,
                 ParticipationPage,
                 PoliticalPage,
+                DemonstrationPage,
                 EndPage]
